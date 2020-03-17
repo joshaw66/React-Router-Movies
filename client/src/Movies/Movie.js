cebf87eaff390270398cd783feead106cc6cbceb
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-17 13:03:30 -0600

    Made the home button link

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,10 +5,8 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
+    const id = props.match.params.id
+    
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
